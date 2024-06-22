ad5bb895aabb5757f4382172c6d9cd61b07de94e
Mykola Lebid <realkolos1@gmail.com>
2024-06-22 03:25:22 +0300

    format date

--- app/api/users/[id]/exercises/route.ts
+++ app/api/users/[id]/exercises/route.ts
@@ -1,6 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
 import { client } from "@/db";
 import { exerciseResponseFactory } from "@/utils";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
   req: NextRequest,
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,7 @@
 import { Exercise, User } from "@prisma/client";
 
+import { format } from "date-fns";
+
 export function markIdWithUnderscore({ id, ...rest }: { id: string }) {
   return { ...rest, _id: id };
 }
@@ -11,8 +13,12 @@ export function exerciseResponseFactory(
 ) {
   return {
     ...markIdWithUnderscore(exercise.user),
-    date: exercise.date,
+    date: formatDate(exercise.date.toString()),
     duration: exercise.duration,
     description: exercise.description,
   };
+}
+
+export function formatDate(input: string, formatStr = "EEE MMM dd yyyy") {
+  return format(new Date(input), formatStr);
 }
